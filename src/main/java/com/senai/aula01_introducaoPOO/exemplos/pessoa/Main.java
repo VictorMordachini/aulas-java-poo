ee369416ae22cd35b2e4065e027e2b2b784d0013
Victor Mordachini <mordachini.v@gmail.com>
2025-01-29 09:27:28 -0300

    29/01 - More examples

--- src/main/java/com/senai/aula01_introducaoPOO/exemplos/pessoa/Main.java
+++ src/main/java/com/senai/aula01_introducaoPOO/exemplos/pessoa/Main.java
@@ -2,9 +2,33 @@ package com.senai.aula01_introducaoPOO.exemplos.pessoa;
 
 public class Main {
     public static void main(String[] args) {
-        Pessoa pessoa1 = new Pessoa("Victor", 19);
+        Pessoa pessoa1 = new Pessoa(
+                "Victor",
+                20);
+        pessoa1.endereco = new Endereco(
+                "Leandro de Sevilha",
+                369,
+                "Parque Novo Lar",
+                "São Paulo",
+                "SP");
+        pessoa1.altura = 1.66F;
 
-        System.out.println(pessoa1.nome + ", " + pessoa1.idade + ", " + pessoa1.altura + ", " +
-                pessoa1.endereco);
+        Pessoa pessoa2 = new Pessoa(
+                "Laura",
+                18,
+                1.65F,
+                new Endereco(
+                        "Adalgiza Uchoa Santos Dumont",
+                        32,
+                        "Jardim Guapira",
+                        "São Paulo",
+                        "SP"));
+
+        System.out.println(pessoa1);
+        System.out.println(pessoa2);
+
+        pessoa1.comer("Hamburguer");
+        pessoa2.comer("Salada");
+
     }
 }
--- src/main/java/com/senai/aula01_introducaoPOO/exemplos/pessoa/Pessoa.java
+++ src/main/java/com/senai/aula01_introducaoPOO/exemplos/pessoa/Pessoa.java
@@ -4,7 +4,14 @@ public class Pessoa {
     String nome;
     int idade;
     float altura;
-    String endereco;
+    Endereco endereco;
+
+    public Pessoa(String nome, int idade, float altura, Endereco endereco) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.endereco = endereco;
+    }
 
     public Pessoa(String nome, int idade) {
         this.nome = nome;
@@ -13,10 +20,16 @@ public class Pessoa {
     }
 
     public void falar(String frase) {
-        System.out.println("A pessoa falou: " + frase);
+        System.out.println(nome + " falou: " + frase);
     }
 
     public void comer(String alimento) {
-        System.out.println("A pessoa comeu: " + alimento);
+        System.out.println(nome + " comeu " + alimento);
+    }
+
+    @Override
+    public String toString() {
+        return "O nome da pessoa é " + this.nome + ", tem " + this.idade +
+                " anos, " + this.altura + " de altura e mora no endereço " + this.endereco;
     }
 }
