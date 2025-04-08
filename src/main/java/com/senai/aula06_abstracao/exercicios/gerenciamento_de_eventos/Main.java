f3ee100003f7b3fbcd8e9b062a2a28bed6e94082
VictorMordachini <mordachini.v@gmail.com>
2025-04-08 08:08:27 -0300

    08/04/25 - Exercicios Abstracao

---
+++ src/main/java/com/senai/aula06_abstracao/exercicios/gerenciamento_de_eventos/Main.java
@@ -0,0 +1,4 @@
+package com.senai.aula06_abstracao.exercicios.gerenciamento_de_eventos;
+
+public class Main {
+}
---
+++ src/main/java/com/senai/aula06_abstracao/exercicios/sistema_padronizado_checkin/Main.java
@@ -0,0 +1,7 @@
+package com.senai.aula06_abstracao.exercicios.sistema_padronizado_checkin;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
